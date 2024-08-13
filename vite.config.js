b50fa1e31e734d53be1687dc37fb3bf539baa4e1
kushal129 <21bmiit129@gmail.com>
2024-08-13 13:43:51 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-paginate']
-    }
+  optimizeDeps: {
+    include: ['react-paginate']
   }
 });
