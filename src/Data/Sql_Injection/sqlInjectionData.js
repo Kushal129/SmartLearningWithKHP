1321f489cb2563f17e33898dc3002f1b5d64f228
kushal129 <21bmiit129@gmail.com>
2024-09-06 13:35:34 +0530

    init sql lebs

--- src/Data/Sql_Injection/sqlInjectionData.js
+++ src/Data/Sql_Injection/sqlInjectionData.js
@@ -1,6 +1,7 @@
 import codeimg1 from '../../Data/Sql_Injection/DVWAimg/codeimg1.png';
 import codeimg2 from '../../Data/Sql_Injection/DVWAimg/codeimg2.png';
 import id2img from '../../Data/Sql_Injection/DVWAimg/id2img.png';
+import sql_leb_img1 from '../../Data/Sql_Injection/sqllebsimg/s1.png';
 
 const sqlInjectionData = [
     {
@@ -100,6 +101,60 @@ const sqlInjectionData = [
         ]
 
     },
+
+    {
+        id: 3,
+        title: " Sql Injection Vulnerability Attack Using Lab's",
+        description: "We install a Lab into the Metasploit 2. and we can find vernability into that system.",
+        despoints: [
+            `1. First Open kali linux then open this link <a href="https://github.com/Audi-1/sqli-labs.git" class="px-1 rounded-md text-purple-500">Click.</a> and <span class="bg-gray-300 p-1 rounded-lg text-red-400">Note: got to <span class="bg-green-500 text-white rounded-lg px-1"><> code </span></span> button then click the <span class="font-bold">Download ZIP</span> option.`,
+
+
+            '2. Now open terminal and give super user prmination. then write this Commands <span class="font-bold">cd Downloads</span>. Then enter this Command <span class="font-bold">sudo mv sqli-labs-master.zip /var/www/html/ </span>',
+
+            '3. Then open terminal and changw the directory to <span class="font-bold">sudo cd /var/www/</span>. then start the service apache2 using this command <span class="font-bold">service apache2 start</span>',
+
+            '4. Then enter this command <span class="font-bold">ifconfig</span> then its show interface list inside of you can consider eth0 and copy the Ip address <span class="font-bold ">ex. 192.168.?.?</span>.',
+
+            `5. Then Open Metasploitable-2 machine  & Login default username and password is <span class="font-bold">msfadmin</span> | if you dont't have ? <a href="https://sourceforge.net/projects/metasploitable/files/latest/download" class="text-purple-500">Download.</a>`,
+
+            `6. Then goto cd /var/www/ and Give sudo permission <span class="font-bold">sudo -s </span> . then ping the ip address <span class="font-bold">192.168.?</span> if you can see the response. then good to go.`,
+
+            `7. Now we download the file from kali linux using this command <span class="font-bold">wget http://(ip)/sqli-labs-master.zip</span> then Verify it is download or not using <span class="font-bold">ls</span> command. then Unzip file <span class="font-bold">unzip sqli-labs-master.zip</span>.`,
+
+            `8. Now Successfully install labs into Metasploitable machine. if we can use this then start Metasploitable and copy the ip address. then go to other machine and open browser and write Metasploitable ip address and /sqli-labs-master or copy this. <span class="font-bold">http://192.168.?.?/sqli-labs-master/</span>`,
+
+        ],
+        pointsTitle: "Use sql-labs-master stap by stap.",
+        points: [
+            `1. <span class="text-red-700">Note: If you first time open must follow this </span> first go to <span class="text-pink-400">Setup/reset Database for labs</span> then go back and downside we can show all labs.`,
+
+            `<span class="font-bold">Now you can find Vulnerability into all this machines.</span>`
+        ],
+        code: [],
+        note: "",
+        subdesc: "",
+        images: [
+            {
+                src: sql_leb_img1,
+                alt: "sql_leb_img."
+            },
+        ]
+    },
+
+
+    // {
+    //     id:,
+    //     title: "",
+    //     description: ".",
+    //     despoints: [],
+    //     pointsTitle: "",
+    //     points: [],
+    //     code: [],
+    //     note: "",
+    //     subdesc: "",
+    //     images: []
+    // },
 
 
 ];
